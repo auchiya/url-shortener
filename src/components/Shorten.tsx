810ba7ec2428d717888f3885a91804c691571dae
aUchiya <Agustin.Uchiya@derivco.co.uk>
2023-09-21 14:39:18 +0200

    link style and url validation

--- src/components/Shorten.tsx
+++ src/components/Shorten.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import ThemeSwitch from './ThemeSwitch';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   display: flex;
@@ -42,18 +43,47 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
+export const LinkTo = styled(Link)`
+ color:${(props) => props.theme.color};;
+ margin: 10px;
+&:hover,
+&:focus{
+    color: ${(props) => props.theme.color};;
+}
+&:active{
+    color: red;
+};
+`
+
 const Shorten = () => {
   const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
 
+  const isValidUrl = (urlInput: string) => {
+    try {
+      new URL(urlInput);
+    } catch (_) {
+      return false;
+    }
+    return true;
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      const response = await axios.post('/shorten', { url });
-      setShortUrl(response.data);
-    } catch (error) {
-      console.error(error);
+
+    if (url === '') {
+      alert('URL cannot be empty');
+    } else if (!isValidUrl(url)) {
+      alert('URL is not valid');
+    } else {
+      try {
+        const response = await axios.post('/shorten', { url });
+        setShortUrl(`${window.location.origin}/${response.data.code}`);
+      } catch (error) {
+        console.error(error);
+      }
     }
+
   };
 
   return (
@@ -70,7 +100,7 @@ const Shorten = () => {
           />
           <Button type="submit">Shorten</Button>
         </Form>
-        {shortUrl && <p>Short URL: {shortUrl}</p>}
+        {shortUrl && <p>Short URL: <LinkTo to={shortUrl} target='_blank'>{shortUrl}</LinkTo></p>}
       </Container>
     </>
   );
